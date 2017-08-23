a44e6661dbabda5c9683e6129cd173a12ce67067
Michael Chiang <michael.chiang25@gmail.com>
2017-08-23 14:38:29 -0400

    Created .js files for wordbank. Included Jquery

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var correct = 0;
+	var incorrect = 0;
+	var score = 0;
+
+
+	console.log(bank[0])
+
+
+});
---
+++ js/wordbank.js
@@ -0,0 +1,13 @@
+	var bank = [{
+	        question: "What is 2+2?",
+	        list: ["4", "6", "10", "22"],
+	        difficulty: "easy",
+	        answer: "4"
+	    },
+	    {
+	        question: "How many floors does does the Eiffel Tower have?",
+	        list: ["1", "99", "3", "10"],
+	        difficulty: "hard",
+	        answer: "3"
+	    }
+	]
